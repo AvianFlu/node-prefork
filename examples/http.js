af276a957ee74467d371a182aee68e4a8e60a024
Charlie McConnell <charlie@charlieistheman.com>
2012-05-15 09:52:12 -0700

    [docs] Update http example for changed api.

--- examples/http.js
+++ examples/http.js
@@ -2,8 +2,13 @@
 var prefork = require('../lib/prefork');
 var http = require('http');
 
-prefork();
+prefork({
+  stdout: 'out.log',
+  stderr: 'err.log'
+});
 
 http.createServer(function (req, res) {
+  console.log('stdout still running.');
+  console.error('stderr still running.');
   res.end('I am still running');
 }).listen(9090);
