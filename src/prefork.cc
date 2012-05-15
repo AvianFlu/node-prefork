30d7118b86971f20d65cea02d5b6258bd50fa49b
Charlie McConnell <charlie@charlieistheman.com>
2012-05-15 09:50:23 -0700

    [refactor] prefork.cc now works with fds instead of strings.

--- src/prefork.cc
+++ src/prefork.cc
@@ -1,59 +1,52 @@
 #include <unistd.h>
 #include <v8.h>
 #include <node.h>
+#include <assert.h>
+#include <errno.h>
 #include "../deps/aeternum/aeternum.h"
 
 using namespace v8;
 using namespace node;
 
-const char* ToCString(const String::Utf8Value& value) {
-  return *value ? *value : "<string conversion failed>";
-}
-
 static Handle<Value> Prefork(const Arguments& args) {
   HandleScope scope;
+  
+  int stdio_fds[3] = { -1, -1, -1 };
   // Take the options object out of args[0]
   Local<Object> js_options = args[0]->ToObject();
-
-  // Grab stdio filenames from the options object
-  Local<Value> stdin_v = js_options->Get(String::NewSymbol("stdin"));
-  Local<Value> stdout_v = js_options->Get(String::NewSymbol("stdout"));
-  Local<Value> stderr_v = js_options->Get(String::NewSymbol("stderr"));
+  // Trying to ->Get() properties of empty objects will segfault.
+  if (!js_options.IsEmpty()) {
+    Local<Value> custom_fds_v = js_options->Get(String::NewSymbol("customFds"));
+    if (!custom_fds_v.IsEmpty() && custom_fds_v->IsArray()) {
+      Local<Array> custom_fds = Local<Array>::Cast(custom_fds_v);
+      int i;
+      for(i = 0; i < 3; i++) {
+        stdio_fds[i] = custom_fds->Get(i)->Int32Value();
+      }
+    }
+  }
 
   // fork(2) and setsid()
-  aeternum_fork();
+  if (aeternum_fork() < 0) {
+    return ThrowException(ErrnoException(errno, "aeternum_fork()"));
+  }
   // let the event loop know it happened
   ev_default_fork();
 
-  // tediously convert javascript strings to C strings, in three steps.
-  // redirect stdio to the file provided, if provided.
-  if (stdin_v->IsString()) {
-    String::Utf8Value infile(stdin_v);
-    aeternum_redirect(ToCString(infile), STDIN_FILENO);
+  if (aeternum_dup(stdio_fds[0], STDIN_FILENO) == -1) {
+    return ThrowException(ErrnoException(errno, "aeternum_dup()"));
   }
-  else {
-    aeternum_redirect(NULL, STDIN_FILENO);
+  if (aeternum_dup(stdio_fds[1], STDOUT_FILENO) == -1) {
+    return ThrowException(ErrnoException(errno, "aeternum_dup()"));
   }
-
-  if (stdout_v->IsString()) {
-    String::Utf8Value outfile(stdout_v);
-    aeternum_redirect(ToCString(outfile), STDOUT_FILENO);
-  }
-  else {
-    aeternum_redirect(NULL, STDOUT_FILENO);
-  }
-
-  if (stderr_v->IsString()) {
-    String::Utf8Value errfile(stderr_v);
-    aeternum_redirect(ToCString(errfile), STDERR_FILENO);
-  }
-  else {
-    aeternum_redirect(NULL, STDERR_FILENO);
+  if (aeternum_dup(stdio_fds[2], STDERR_FILENO) == -1) {
+    return ThrowException(ErrnoException(errno, "aeternum_dup()"));
   }
 
   // return null, since any error would have made things blow up by now.
   return scope.Close(Null());
 }
+
 
 
 extern "C" void init(Handle<Object> target) {
